a4187bfa4acc0cdc28cdbdec01aba4539cf8ac41
Diego Cardoso <dev.diegocardoso@outlook.com>
2023-05-24 20:20:43 +0200

    Practice done

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,16 @@ import (
 )
 
 func main() {
-	fmt.Println("Structs")
+	poodle := Dog{"Poodle", 10}
+	fmt.Println(poodle)
+	fmt.Printf("%+v\n", poodle)
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Age)
+	poodle.Age = 9
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Age)
+
+}
+
+type Dog struct {
+	Breed string
+	Age   int
 }
