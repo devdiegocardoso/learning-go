94d7f27538543b8cd06d669e56139d570d125d12
Diego Cardoso <dev.diegocardoso@outlook.com>
2023-05-24 19:40:27 +0200

    Practice done

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,15 @@ import (
 )
 
 func main() {
-	fmt.Println("Arrays")
+	var colors [3]string
+	colors[0] = "Red"
+	colors[1] = "Green"
+	colors[2] = "Blue"
+	fmt.Println(colors)
+	fmt.Println(colors[1])
+
+	var numbers = [5]int{5, 10, 15, 20, 25}
+	fmt.Println(numbers)
+
+	fmt.Println("Num of colors:", len(colors))
 }
